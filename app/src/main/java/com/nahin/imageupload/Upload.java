ffd67a485716d0b68554cd20e5abbd48cd55e092
Nahin-CDR <dev.nahin95@gmail.com>
2020-03-16 15:46:51 +0600

    //first

---
+++ app/src/main/java/com/nahin/imageupload/Upload.java
@@ -0,0 +1,19 @@
+package com.nahin.imageupload;
+
+public class Upload {
+
+    private String mImageUrl;
+
+    public Upload(){
+
+        //Empty Constructor
+    }
+
+    public Upload(String mImageUrl) {
+        this.mImageUrl = mImageUrl;
+    }
+
+    public String getmImageUrl() {
+        return mImageUrl;
+    }
+}
